4757928586f6fa130114dcec96420c97fc748d42
CHRISTOPHER KELLY <christopherkelly@engi.network>
2022-09-15 10:50:54 -0700

    testing delete

---
+++ PrimeService/bar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i < (int)Math.Round(Math.Sqrt(candidate)) + 1; i++)
+            {
+                if ((candidate % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
